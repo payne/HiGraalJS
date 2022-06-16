58ff804072120b911d9361635233f6fceacf5c2a
Matt Payne <Payne@MattPayne.org>
2022-06-16 03:24:44 +0000

    woot! Strings now come back too.  Nice.

--- Main.java
+++ Main.java
@@ -96,14 +96,15 @@ public static void run() throws Exception {
 }
 
 
-    static String JS_CODE = "(function myFun(param){console.log('hello '+param);})";
+  static String JS_CODE = "(function myFun(param){console.log('hello '+param); return 'fun ' + param; })";
 
 // Based on https://docs.oracle.com/en/graalvm/enterprise/20/docs/reference-manual/js/
     public static void demo2(String arg) {
         System.out.println("Hello Java!");
         try (Context context = Context.create()) {
             Value value = context.eval("js", JS_CODE);
-            value.execute(arg);
+            Object result = value.execute(arg);
+          System.out.println(result);
         }
     }
 
