7e2a9f42b27112ab79754bbd0c4713e7c55b1bad
Matt Payne <Payne@MattPayne.org>
2022-06-18 02:40:34 +0000

    return copy of value coming back from JavaScript

--- Main.java
+++ Main.java
@@ -68,7 +68,9 @@ class Main {
   
   public static void main(String[] args) {
     System.out.println("Hello world!");
-  demo2("eggs");
+  Object result = demo2("eggs");
+    System.out.format("result is a %s and has value:\n%s\n", 
+                     result.getClass().getName(), result);
     try { 
     run();
     } catch (Exception e) {
@@ -99,12 +101,13 @@ public static void run() throws Exception {
   static String JS_CODE = "(function myFun(param){console.log('hello '+param); return 'fun ' + param; })";
 
 // Based on https://docs.oracle.com/en/graalvm/enterprise/20/docs/reference-manual/js/
-    public static void demo2(String arg) {
+    public static Object demo2(String arg) {
         System.out.println("Hello Java!");
         try (Context context = Context.create()) {
             Value value = context.eval("js", JS_CODE);
             Object result = value.execute(arg);
           System.out.println(result);
+          return result.toString();
         }
     }
 
